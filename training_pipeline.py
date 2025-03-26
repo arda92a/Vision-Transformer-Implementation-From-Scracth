ad26e886563d32a920b8fbd88f0ec67dcf570b5d
“[arda92a]” <[ardaoztuner@ogr.eskisehir.edu.tr]>
2025-03-26 22:56:55 +0300

    add max gradient norm parameter

--- training_pipeline.py
+++ training_pipeline.py
@@ -16,8 +16,9 @@ IMAGE_SIZE = (224,224)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 PATCH_SIZE = 16
 IN_CHANNELS = 3
-EPOCHS = 10
+EPOCHS = 3
 SEED = 42
+MAX_GRAD_NORM = 1.0
 
 torch.manual_seed(SEED)
 random.seed(SEED)
@@ -100,7 +101,8 @@ result_with_gradient_clipping = train_With_Gradient_Clipping(model=model_with_gr
                                                        optimizer=optimizer_GC,
                                                        loss_fn=criterion,
                                                        epochs=EPOCHS,
-                                                       device=DEVICE)
+                                                       device=DEVICE,
+                                                       max_grad_norm=MAX_GRAD_NORM)
 
 
 print("[INFO] Training with Gradient Clipping End...")
